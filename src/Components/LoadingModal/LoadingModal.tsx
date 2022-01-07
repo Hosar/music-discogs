ef2d62a42b16cddbc2885de9dbd4d5ac49e66663
Hosar <hosarsiph_hernandez@vfc.com>
2022-01-07 14:08:15 -0600

    Removing not necessary code

--- src/Components/LoadingModal/LoadingModal.tsx
+++ src/Components/LoadingModal/LoadingModal.tsx
@@ -6,7 +6,7 @@ export function LoadingModal({ isLoading }: { isLoading: boolean }) {
     return (
         <>
             <div style={styles.modalStyle}>
-                Loading ...??
+                Loading ...
             </div>
         </>
     );
--- src/api/discogsMusic.api.ts
+++ src/api/discogsMusic.api.ts
@@ -1,27 +1,8 @@
-import { Axios } from 'axios';
-import * as request from 'superagent';
-
 export const getRecordsByArtist = (httpProvider: any) => (artist: string, page = 1) => {
-    // const url = 'https://api.discogs.com/database/search';
-    console.log('...............>', artist);
-    console.log('...............>', httpProvider);
-    // httpProvider.hostUrl.pathName = 'database/search';
-    // const theQuery = httpProvider.query(`q=${artist}`)
-    // console.log(theQuery);
-    // const url = new URL('https://api.discogs.com/database/search')
     let params = httpProvider.hostUrl.searchParams
     params.set('per_page',10);
     params.set('q',artist);
     params.set('page',page);
     httpProvider.hostUrl.search = params.toString();
-    console.log('httpProvider.hostUrl', httpProvider.hostUrl)
-    console.log('httpProvider.hostUrl', httpProvider.hostUrl.href)
-    // return fetch(httpProvider.hostUrl).then((response: any) => response.json());
     return fetch(httpProvider.hostUrl).then((response: any) => response.json()); 
 }
-
-
-
-// export const getRecordsByArtist = (httpProvider: Axios) => (artist: string) => {
-//     return httpProvider.get(`database/search?q=${artist}&type=artist&token=FFScUljnRjxkVXRMbNyXmIFjNDzttPFDLrejBLta`);
-// }
